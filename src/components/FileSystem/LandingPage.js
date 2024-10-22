237ac2b27a557cce912eaa54d381757fd3178908
C Sarath <sarathyadav112@gmail.com>
2024-10-23 01:03:22 +0530

    updated Ui

--- src/components/FileSystem/LandingPage.js
+++ src/components/FileSystem/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, SimpleGrid, Button, Heading, Container, Text, VStack, useColorModeValue } from '@chakra-ui/react';
+import { Box, SimpleGrid, Button, Heading, Container, Text, VStack, useColorModeValue, Flex } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { FaTerminal, FaFolder, FaFile, FaCopy, FaTrash, FaSearch, FaUserLock, FaNetworkWired } from 'react-icons/fa';
 
@@ -21,22 +21,24 @@ const commands = [
 
 const CommandButton = ({ cmd }) => {
   const bg = useColorModeValue('white', 'gray.800');
-  const hoverBg = useColorModeValue('gray.100', 'gray.700');
+  const hoverBg = useColorModeValue(`${cmd.color}50`, `${cmd.color}900`);
+  const textColor = useColorModeValue('gray.800', 'white');
 
   return (
     <Link to={`/command/${cmd.name}`}>
       <Button
         width="100%"
-        height="100px"
+        height={{ base: "80px", md: "100px" }}
         bg={bg}
-        _hover={{ bg: hoverBg }}
+        _hover={{ bg: hoverBg, transform: 'translateY(-5px)' }}
         boxShadow="md"
         borderRadius="lg"
         transition="all 0.3s"
+        borderLeft={`4px solid ${cmd.color}`}
       >
         <VStack spacing={2}>
           <cmd.icon size="24" color={cmd.color} />
-          <Text fontWeight="bold">{cmd.name}</Text>
+          <Text fontWeight="bold" color={textColor}>{cmd.name}</Text>
         </VStack>
       </Button>
     </Link>
@@ -45,18 +47,22 @@ const CommandButton = ({ cmd }) => {
 
 const LandingPage = () => {
   const bg = useColorModeValue('gray.50', 'gray.900');
+  const headingColor = useColorModeValue('blue.600', 'blue.300');
+  const textColor = useColorModeValue('gray.600', 'gray.300');
 
   return (
     <Box bg={bg} minHeight="100vh" py={10}>
       <Container maxW="container.xl">
         <VStack spacing={8} align="stretch">
-          <Heading as="h1" size="2xl" textAlign="center">
-            Linux Command Visualizer
-          </Heading>
-          <Text fontSize="xl" textAlign="center">
-            Click on a command to learn more and see it in action!
-          </Text>
-          <SimpleGrid columns={{ base: 2, sm: 3, md: 4, lg: 6 }} spacing={6}>
+          <Flex direction="column" align="center" mb={8}>
+            <Heading as="h1" size="2xl" textAlign="center" color={headingColor} mb={4}>
+              Linux Command Visualizer
+            </Heading>
+            <Text fontSize={{ base: "lg", md: "xl" }} textAlign="center" color={textColor} maxW="600px">
+              Explore and learn Linux commands interactively. Click on a command to see it in action!
+            </Text>
+          </Flex>
+          <SimpleGrid columns={{ base: 2, sm: 3, md: 4, lg: 5, xl: 6 }} spacing={{ base: 4, md: 6 }}>
             {commands.map((cmd) => (
               <CommandButton key={cmd.name} cmd={cmd} />
             ))}
